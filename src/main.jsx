8053fdea37a2ee05a9cda70db03fa969d76a2ccf
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-26 17:53:47 +0300

    2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+
+//     <App />
+ 
+// )
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+import { Provider } from 'react-redux';
+import store from './store'; // импортируйте ваш store
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+            <Provider store={store}>
+                    <App />
+            </Provider>
+    </React.StrictMode>
 
-    <App />
- 
 )
