c2b06e94385976a921bb6f8a296f0159ac4be8e3
Taisiia777 <sporyshev.savelii@gmail.com>
2024-08-28 20:31:27 +0300

    20

--- src/components/TapBlock.jsx
+++ src/components/TapBlock.jsx
@@ -132,23 +132,10 @@ useEffect(() => {
  onClick={handleClick}
  onTouchStart={(e) => setTouch(e.touches)}
  onTouchEnd={handleClick}
-// onClick={(e)=>{
-//   console.log(e);
-//   console.log(e.target.getBoundingClientRect());
-  
-//   if (!document.querySelector('.circle-feedback'))return;
-// var child = document.createElement('nav');
-// child.style.top = e.clientY-e.target.getBoundingClientRect().y + 'px';
-// child.style.left = e.clientX-e.target.getBoundingClientRect().x + 'px';
-// child.textContent = '+1'; 
-// child.style.position = 'absolute';
-//   document.querySelector('.circle-feedback').appendChild(child);
-//  setTimeout(()=>{ child.remove() }, 1000);
-// }} 
 className="circle" >
-      {swimmers.map((v, i) => 
+<div className="circle-feedback"> {swimmers.map((v, i) => 
   <ClickerSwimmer key={i} swimmers={swimmers} setSwimmers={setSwimmers} {...v} />
-  )}
+  )} </div>
     <div className="inner"
     >
 
